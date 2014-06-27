762a7a16004d2a57f5f6f8bed7997f83e0593e8a
Camilo Sanchez <camilin87@gmail.com>
2014-06-27 01:16:34 -0400

    refine a lil bit more the sample

--- scrap/forecastapi.py
+++ scrap/forecastapi.py
@@ -2,36 +2,32 @@ import forecastio
 import datetime
 from datetime import datetime
 
-
-def main():
+def call_api(lat, lng):
     api_key = "151cfe4d4aed6cd467097090a9250dab"
-    # zipcode 33012
-    # 323|25.86|-80.3|25.86,-80.3|25.86%2C-80.3|-5|1
-    lat = 25.86
-    lng = -80.30
     time = datetime.now().replace(minute=0, second=0, microsecond=0)
 
+    # WARNING the time is not being used, thus the first API call is being made
     forecast = forecastio.load_forecast(api_key, lat, lng)
 
-    # print(forecast.hourly().data[0].temperature)
-    # print("===========Hourly Data=========")
-    # by_hour = forecast.hourly()
-    # print("Hourly Summary: %s" % (by_hour.summary))
-
-    # for hourly_data_point in by_hour.data:
-    #     print(hourly_data_point)
-
-    # print("===========Daily Data=========")
-    # by_day = forecast.daily()
-    # print("Daily Summary: %s" % (by_day.summary))
-
-    # for daily_data_point in by_day.data:
-    #     print(daily_data_point)
-
-    print("=========Current Data=========")
+    print("\n=========Current Data=========", lat, ", ", lng)
     datapoint = forecast.currently()
-    for k, v in datapoint.__dict__["d"].items():
-        print(str(k).rjust(20), " => ", v)
+
+    print("Time:", datapoint.time)
+
+    for k, v in datapoint.__dict__.items():
+        if k != "d":
+            print(k, " => ", v)
+
+    dict_data = datapoint.__dict__["d"]    
+    for k in sorted(dict_data):
+        print(str(k).rjust(20), " => ", dict_data[k])
+
+def main():
+    # 33012 hialeah
+    call_api(25.86, -80.30)
+
+    # 90045 LAX
+    call_api(33.96, -118.39)
 
 if __name__ == "__main__":
     main()
