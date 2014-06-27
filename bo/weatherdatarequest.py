083923a777625d60a12dc87f0ebddbc9669e60ea
Camilo Sanchez <camilin87@gmail.com>
2014-06-27 19:28:07 -0400

    latitude boundaries validation

--- bo/weatherdatarequest.py
+++ bo/weatherdatarequest.py
@@ -1,2 +1,4 @@
 class WeatherDataRequest(object):
-    pass
+    def __init__(self, latitude, longitude):
+        if latitude < -90.0 or latitude > 90.0:
+            raise ValueError("Latitude out of bounds")
--- test/test_weatherdatarequest.py
+++ test/test_weatherdatarequest.py
@@ -4,4 +4,12 @@ from bo.weatherdatarequest import WeatherDataRequest
 
 class TestWeatherDataRequest(TestCase):
     def test_creation(self):
-        self.assertIsNotNone(WeatherDataRequest())
+        self.assertIsNotNone(WeatherDataRequest(12.34, 56.70))
+
+    def test_validates_latitude_min_boundary(self):
+        with self.assertRaises(ValueError):
+            WeatherDataRequest(-90.0 - 0.1, 56.70)
+
+    def test_validates_latitude_max_boundary(self):
+        with self.assertRaises(ValueError):
+            WeatherDataRequest(90.0 + 0.1, 56.70)
