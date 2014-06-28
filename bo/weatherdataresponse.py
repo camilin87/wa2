76d192af26a13c6e5fdc29a270bb9aa12de8789b
Camilo Sanchez <camilin87@gmail.com>
2014-06-27 20:26:06 -0400

    TestWeatherDataResponse creation

--- bo/weatherdataresponse.py
+++ bo/weatherdataresponse.py
@@ -1,2 +1,5 @@
 class WeatherDataResponse(object):
-    pass
+    def __init__(self, summary_str, precip_intensity, precip_probability):
+        self.summary_str = summary_str
+        self.precip_probability = precip_probability
+        self.precip_intensity = precip_intensity
--- test/test_weatherdataresponse.py
+++ test/test_weatherdataresponse.py
@@ -4,4 +4,16 @@ from bo.weatherdataresponse import WeatherDataResponse
 
 class TestWeatherDataResponse(TestCase):
     def test_creation(self):
-        self.assertIsNotNone(WeatherDataResponse())
+        precipitation_probability = 2.34
+        precipitation_intensity = 1.3
+        summary = "it will rain a fucking lot"
+
+        response = WeatherDataResponse(
+            summary,
+            precipitation_intensity,
+            precipitation_probability,
+        )
+
+        self.assertEquals(summary, response.summary_str)
+        self.assertEquals(2.3400, response.precip_probability)
+        self.assertEquals(1.3, response.precip_intensity)
