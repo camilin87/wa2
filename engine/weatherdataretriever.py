8b0d7e601c05217118e407c6f3b25b599a6aa701
Camilo Sanchez <camilin87@gmail.com>
2014-06-29 18:04:52 -0400

    correct pep issue

--- engine/weatherdataretriever.py
+++ engine/weatherdataretriever.py
@@ -2,8 +2,8 @@ import abc
 
 
 class WeatherDataRetriever(object):
-    __metaclass__  = abc.ABCMeta
- 
+    __metaclass__ = abc.ABCMeta
+
     @abc.abstractmethod
     def retrieve(self, weather_data_request):
         raise NotImplementedError("This is an abstract class")
