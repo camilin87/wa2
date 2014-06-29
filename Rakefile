41595f8eaa93265c46d8e63015bb5586ea37ed78
Camilo Sanchez <camilin87@gmail.com>
2014-06-28 22:42:09 -0400

    create primitive rakefile

---
+++ Rakefile
@@ -0,0 +1,5 @@
+task :default => [:all]
+
+task :configure_pyenv do
+  print "lolo"
+end
--- setup-dev.sh
+++ setup-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # IMPORTANT: do not add enything else to this file
-# once ant is installed everything should be run through it
+# once rake is installed everything should be run through it
 brew install ruby
 yes N | gem install rake
-# ant configure-pyenv
+rake configure_pyenv
