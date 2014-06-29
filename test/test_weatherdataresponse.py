34cc9bf56aa2d8885b6e95515ea720544a6eaea5
Camilo Sanchez <camilin87@gmail.com>
2014-06-29 16:45:34 -0400

    test_validates_precip_intensity_should_be_numeric

--- test/test_weatherdataresponse.py
+++ test/test_weatherdataresponse.py
@@ -41,3 +41,7 @@ class TestWeatherDataResponse(TestCase):
     def test_validates_precip_intensity_min_value(self):
         with self.assertRaises(ValueError):
             WeatherDataResponse("NA", 0.0 - 0.1, 0.0)
+
+    def test_validates_precip_intensity_should_be_numeric(self):
+        with self.assertRaises(TypeError):
+            WeatherDataResponse("NA", "0.0", 0.0)
