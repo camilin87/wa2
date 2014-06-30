607ab2cdd79c6fb398fd1d5e57f2d82fb0fad603
Camilo Sanchez <camilin87@gmail.com>
2014-06-29 21:55:40 -0400

    declare TestEngineFactory

---
+++ test/test_enginefactory.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from factory.enginefactory import EngineFactory
+
+
+class TestEngineFactory(TestCase):
+    def test_creates_production_weather_data_retriever(self):
+        # EngineFactory.create_weather_data_retriever()
+        pass
