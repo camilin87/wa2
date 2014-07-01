07467c8cf77dc81f4dfb76cff8c863a32f2be570
Camilo Sanchez <camilin87@gmail.com>
2014-06-30 23:55:22 -0400

    test_retrieves_weather_data2

--- test/test_forecastioretriever.py
+++ test/test_forecastioretriever.py
@@ -27,3 +27,29 @@ class TestForecastIoRetriever(TestCase):
         self.assertEquals("some rain", response.summary_str)
         self.assertEquals(1.0, response.precip_probability)
         self.assertEquals(0.8, response.precip_intensity)
+
+    @patch("extapi.forecastioretriever.load_forecast")
+    def test_retrieves_weather_data2(self, forecast_mock):
+        seeded_datapoint = MagicMock()
+        seeded_response = MagicMock()
+
+        def side_effect_forecast(api_key, lat, lng):
+            if api_key == "api key" and lat == 25.86 and lng == -80.3:
+                currently_mock = MagicMock()
+                currently_mock.currently.return_value = seeded_datapoint
+                return currently_mock
+            raise ValueError("invalid api call")
+        forecast_mock.side_effect = side_effect_forecast
+
+        def side_effect_build(p_datatpoint):
+            if p_datatpoint == seeded_datapoint:
+                return seeded_response
+            raise ValueError("invalid build call")
+
+        retriever = ForecastIoRetriever("api key")
+        retriever.builder = MagicMock()
+        retriever.builder.build = MagicMock(side_effect=side_effect_build)
+
+        response = retriever.retrieve(DataRequest(25.86, -80.30))
+
+        self.assertEquals(seeded_response, response)
