0fc21d822637760c77e675441e525aa52e4de91b
Camilo Sanchez <camilin87@gmail.com>
2014-07-01 00:28:34 -0400

    add the rest of the existing dataresponse tests

--- test/test_dataresponsebuilder.py
+++ test/test_dataresponsebuilder.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from extapi.dataresponsebuilder import DataResponseBuilder
 from mock import MagicMock
 from mock import patch
+from bo import intensitytype
 
 
 class TestDataResponseBuilder(TestCase):
@@ -17,6 +18,10 @@ class TestDataResponseBuilder(TestCase):
         seeded_datapoint.precipIntensity = precipIntensity
         seeded_datapoint.precipProbability = precipProbability
         return seeded_datapoint
+
+    def _build_response(self, summary, precipIntensity, precipProbability):
+        datapoint = self._data_point(summary, precipIntensity, precipProbability)
+        return self.builder.build(datapoint)
 
     def test_builds_response(self):
         seeded_datapoint = self._data_point("some rain", 0.8, 1)
@@ -68,3 +73,28 @@ class TestDataResponseBuilder(TestCase):
         with self.assertRaises(TypeError):
             seeded_datapoint = self._data_point("NA", "0.0", 0.0)
             self.builder.build(seeded_datapoint)
+
+    def test_pop_percent(self):
+        self.assertEquals(0, self._build_response("NA", 0, 0).pop_percent)
+        self.assertEquals(50, self._build_response("NA", 0, 0.5).pop_percent)
+        self.assertEquals(99, self._build_response("NA", 0, 0.995).pop_percent)
+
+    def _precip_with_intensity(self, precipitation_intensity):
+        response = self._build_response("NA", precipitation_intensity, 0.0)
+        return response.precipitation
+
+    def test_no_precipitation(self):
+        self.assertEquals(intensitytype.NONE, self._precip_with_intensity(0.0))
+        self.assertEquals(intensitytype.NONE, self._precip_with_intensity(0.002 - 0.0001))
+
+    def test_light_precipitation(self):
+        self.assertEquals(intensitytype.LIGHT, self._precip_with_intensity(0.002))
+        self.assertEquals(intensitytype.LIGHT, self._precip_with_intensity(0.1 - 0.0001))
+
+    def test_moderate_precipitation(self):
+        self.assertEquals(intensitytype.MODERATE, self._precip_with_intensity(0.1))
+        self.assertEquals(intensitytype.MODERATE, self._precip_with_intensity(0.4 - 0.0001))
+
+    def test_heavy_precipitation(self):
+        self.assertEquals(intensitytype.HEAVY, self._precip_with_intensity(0.4))
+        self.assertEquals(intensitytype.HEAVY, self._precip_with_intensity(10))
