c04f5af13b8394588cafab5a4fc8250b7b095817
Camilo Sanchez <camilin87@gmail.com>
2014-07-02 21:00:21 -0400

    defined api return codes

---
+++ api/returncode.py
@@ -0,0 +1,20 @@
+# Return Code classes
+# 0 - 99 Reserved
+
+# 100 - 199 Warnings
+#           Warnings are still successful responses.
+#           However, they need to be taken care of
+OBSOLETE_API = 100
+
+# 200 - 299 Success
+OK = 200
+
+# 300 - 399 Client api key error
+KEY_ERROR = 300
+
+# 400 - 499 Designed Api Errors
+API_SHUTDOWN = 400
+
+# 500 - 599 Unexpected Errors
+UNEXPECTED_ERROR = 500
+EXTERNAL_API_ERROR = 550
