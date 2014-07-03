babf6c717d4416a8a8693db0e3bd28b06b9a20dd
Camilo Sanchez <camilin87@gmail.com>
2014-07-03 01:10:07 -0400

    extract _return_error and _get_internal to remove duplication

--- api/datarequestcontroller.py
+++ api/datarequestcontroller.py
@@ -6,18 +6,60 @@ from api import returncode
 class DataRequestController(object):
     def __init__(self, data_request_builder, key_validator):
         self.builder = data_request_builder
+        self._reset_state()
+
+    def _reset_state(self):
+        self._return_code = None
+        self._error_msg = ""
+        self._summary_str = "NA"
+        self._pop_percent = -1
+        self._intensity_type = -1
+        self._precipitation_type = -1
+
+        self._api_key_str = None
+        self._latitude_str = None
+        self._longitude_str = None
 
     def get(self, api_key_str, latitude_str, longitude_str):
-        request = ApiRequest(api_key_str, latitude_str, longitude_str)
+        self._reset_state()
+
+        self._api_key_str = api_key_str
+        self._latitude_str = latitude_str
+        self._longitude_str = longitude_str
+
+        try:
+            self._get_internal()
+        finally:
+            return ApiResponse(
+                self._return_code,
+                self._error_msg,
+                self._summary_str,
+                self._pop_percent,
+                self._intensity_type,
+                self._precipitation_type
+            )
+
+    def _get_internal(self):
+        request = ApiRequest(
+            self._api_key_str,
+            self._latitude_str,
+            self._longitude_str
+        )
         validation_result = request.validate()
 
         if validation_result != returncode.OK:
-            return ApiResponse(validation_result, "Invalid Request Parameters", "NA", -1, -1, -1)
+            self._return_error(validation_result, "Invalid Request Parameters")
+            return
 
         data_request = None
         try:
             data_request = self.builder.build(request)
         except ValueError as err:
-            return ApiResponse(returncode.ERROR_BUILDING_REQUEST, str(err), "NA", -1, -1, -1)
+            self._return_error(returncode.ERROR_BUILDING_REQUEST, str(err))
+            return
 
-        return ApiResponse(returncode.INVALID_API_KEY, "Invalid Api Key", "NA", -1, -1, -1)
+        self._return_error(returncode.INVALID_API_KEY, "Invalid Api Key")
+
+    def _return_error(self, return_code, error_message):
+        self._return_code = return_code
+        self._error_msg = error_message
