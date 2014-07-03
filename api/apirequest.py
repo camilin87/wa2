4896b7d163fb0a85922472199287d77c20944d6d
Camilo Sanchez <camilin87@gmail.com>
2014-07-02 21:21:31 -0400

    apirequest test_creation

--- api/apirequest.py
+++ api/apirequest.py
@@ -1,2 +1,5 @@
 class ApiRequest(object):
-    pass
+    def __init__(self, api_key, lat_str, long_str):
+        self.api_key = api_key
+        self.latitude_str = lat_str
+        self.longitude_str = long_str
--- test/test_apirequest.py
+++ test/test_apirequest.py
@@ -4,6 +4,9 @@ from api.apirequest import ApiRequest
 
 class TestApiRequest(TestCase):
     def test_creation(self):
-        request = ApiRequest()
+        request = ApiRequest("api key", "234.30", "34.99")
 
         self.assertIsNotNone(request)
+        self.assertEquals("api key", request.api_key)
+        self.assertEquals("234.30", request.latitude_str)
+        self.assertEquals("34.99", request.longitude_str)
