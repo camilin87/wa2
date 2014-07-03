b90f830cd419c7699850fce4e0afbc95fc34f2d6
Camilo Sanchez <camilin87@gmail.com>
2014-07-03 00:12:01 -0400

    test_returns_invalid_request_validation_error_code

--- api/datarequestcontroller.py
+++ api/datarequestcontroller.py
@@ -1,3 +1,12 @@
+from api.apirequest import ApiRequest
+from api.apiresponse import ApiResponse
+
+
 class DataRequestController(object):
     def __init__(self, data_request_builder, key_validator):
         pass
+
+    def get(self, api_key_str, latitude_str, longitude_str):
+        request = ApiRequest(api_key_str, latitude_str, longitude_str)
+        validation_result = request.validate()
+        return ApiResponse(validation_result, "", "NA", -1, -1, -1)
--- test/test_datarequestcontroller.py
+++ test/test_datarequestcontroller.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from unittest.mock import MagicMock
 from api.datarequestcontroller import DataRequestController
+from api import returncode
+from api.apirequest import ApiRequest
 
 
 class TestDataRequestController(TestCase):
@@ -11,3 +13,10 @@ class TestDataRequestController(TestCase):
 
     def test_can_be_created(self):
         self.assertIsNotNone(self.controller)
+
+    def test_returns_invalid_request_validation_error_code(self):
+        expected_returncode = str(ApiRequest("", "12.23", "23.45").validate())
+
+        response = self.controller.get("", "12.23", "23.45")
+
+        self.assertEquals(expected_returncode, response.result)
