9caa7dc86b6575d1dbd8ccb71bd11a37ea6581d1
Camilo Sanchez <camilin87@gmail.com>
2014-07-10 01:12:02 -0400

    display 404 page

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,17 @@
-from bottle import route, run
+from bottle import route
+from bottle import run
+from bottle import error
+from bottle import static_file
+from os.path import dirname
+from os.path import realpath
+from os.path import join
+
+current_dir = dirname(realpath(__file__))
+static_files = join(current_dir, "./static/files/")
+
+@error(404)
+def error_404(error):
+    return static_file("404.html", root=static_files)
 
 @route("/rd/<api_key:re:[a-zA-Z0-9]{1,100}>/<latitude:float>/<longitude:float>")
 def retrieve_data(api_key, latitude, longitude):
