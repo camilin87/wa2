b58f6c340953e15b9e706d6c85f55342ea3e737f
Camilo Sanchez <camilin87@gmail.com>
2014-07-10 01:24:05 -0400

    return a json response

--- webapp/app.py
+++ webapp/app.py
@@ -15,6 +15,11 @@ def error_404(error):
 
 @route("/rd/<api_key:re:[a-zA-Z0-9]{1,100}>/<latitude:float>/<longitude:float>")
 def retrieve_data(api_key, latitude, longitude):
-    return "rd/" + api_key + "/" + str(latitude) + "/" + str(longitude)
+    return {
+        "method": "rd",
+        "api_key": api_key,
+        "latitude": latitude,
+        "longitude": longitude
+    }
 
 run(host='0.0.0.0', port=8080, debug=True)
