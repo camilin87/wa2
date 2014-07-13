627be37ed2f06ee4f656bc18a1780c9609ae39c3
Camilo Sanchez <camilin87@gmail.com>
2014-07-13 01:00:20 -0400

    first flask sample app

---
+++ webapp/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return app.send_static_file('404.html'), 404
+
+@app.route("/rd/<api_key>/<float:latitude>/<float:longitude>")
+def retrieve_data(api_key, latitude, longitude):
+    return jsonify({
+        "method": "rd",
+        "api_key": api_key,
+        "latitude": latitude,
+        "longitude": longitude
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
