d06b287826bff34b814a683f73220b7e0417bd57
Camilo Sanchez <camilin87@gmail.com>
2014-07-31 00:18:05 -0400

    improve environment setup

--- test_integration/api_tests.rb
+++ test_integration/api_tests.rb
@@ -1,14 +1,50 @@
 require "rake"
 
-$env_protocol = "http"
+$env_data = {
+    :protocol => "http",
+    :host => "localhost",
+    :port => 80,
+    :validate_cache => false,
+    :cache_params => {
+        :ttl_seconds => 3
+    }
+}
 
 task :default, [:env] do |t, args|
-    $env_protocol = "https"
-    puts "default task called #{args[:env]}"
-
+    configure_environment args[:env]
     Rake::Task["run_tests"].execute
 end
 
+def configure_environment(environment)
+    case environment
+    when "PRODUCTION"
+        set_production_env_data
+    when "STAGING"
+        set_staging_env_data
+    when "DEBUG"
+        set_debug_env_data
+    else
+        raise "invalid environment specified"
+    end
+end
+
+def set_production_env_data
+    $env_data[:protocol] = "https"
+    $env_data[:port] = 443
+    $env_data[:host] = "api.v1.smartweatheralerts.com"
+    $env_data[:validate_cache] = true
+    $env_data[:cache_params][:ttl_seconds] = 3600 - 1
+end
+
+def set_staging_env_data
+    $env_data[:validate_cache] = true
+end
+
+def set_debug_env_data
+    $env_data[:port] = 8080
+end
+
 task :run_tests do
-    puts "run_tests with protocol #{$env_protocol}"
+    puts "run_tests with environment"
+    puts $env_data
 end
