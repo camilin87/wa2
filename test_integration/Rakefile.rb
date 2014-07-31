1e9f9acd471945adbbb649921a904f4583b9ebd9
Camilo Sanchez <camilin87@gmail.com>
2014-07-31 02:07:16 -0400

    start the debug server

--- test_integration/Rakefile.rb
+++ test_integration/Rakefile.rb
@@ -1,5 +1,9 @@
 import "api_tests_environment_setup.rb"
 
+def basedir
+   return File.expand_path ".."
+end
+
 task :default => [:staging]
 
 task :staging do |t|
@@ -11,9 +15,13 @@ task :production do |t|
 end
 
 task :debug do |t|
-    # launch the debug webserver here
+    app_path = File.join(basedir, "webapp/app.py")
+    pid = spawn("python #{app_path}")
+    sleep(3)
+
     run_tests t.name
-    # shutdown the debug webserver here
+
+    Process.kill("HUP", pid)
 end
 
 def run_tests(env_name)
