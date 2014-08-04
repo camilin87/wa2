04806a142ee58e2cc82add40a82d1599053c6375
Camilo Sanchez <camilin87@gmail.com>
2014-08-03 21:08:00 -0400

    extract web app logic to appcore

--- webapp/app.py
+++ webapp/app.py
@@ -1,66 +1,32 @@
 from sys import argv
 from flask import Flask
-from flask import jsonify
-from wa.factory.apifactory import ApiFactory
-from wa.factory.enginefactory import EngineFactory
-from wa.api.freeforall import FreeForAll
-from wa.extapi.citkeys import CitKeys
+from appcore import AppCore
 
-
-disable_debug = False
 app = Flask(__name__)
-
+api_params = "/<api_key>/<latitude>/<longitude>/"
+app_core = AppCore()
 
 @app.errorhandler(404)
 def page_not_found(error):
     return app.send_static_file('404.html'), 404
 
 
-@app.route("/t/<api_key>/<latitude>/<longitude>/")
+@app.route("/t" + api_params)
 def retrieve_data_test(api_key, latitude, longitude):
-    if _disable_debug():
-        abort(404)
-        return
-    return jsonify(ApiFactory.create_dummy_response(float(latitude), float(longitude)).__dict__)
+    return app_core.retrieve_data_test(api_key, latitude, longitude)
 
 
-@app.route("/s/<api_key>/<latitude>/<longitude>/")
+@app.route("/s" + api_params)
 def retrieve_data_staging(api_key, latitude, longitude):
-    if _disable_debug():
-        abort(404)
-        return
-    key_validator = FreeForAll()
-    api_key_reader = CitKeys()
-    return _retrieve_data(api_key, latitude, longitude, key_validator, api_key_reader)
+    return app_core.retrieve_data_staging(api_key, latitude, longitude)
 
 
-def _disable_debug():
-    def _disable_uwsgi_debug():
-        try:
-            from uwsgi import opt
-            return "true" == opt["disable_debug"].decode()
-        except:
-            return False
-    return disable_debug or _disable_uwsgi_debug()
-
-
-@app.route("/p/<api_key>/<latitude>/<longitude>/")
+@app.route("/p" + api_params)
 def retrieve_data_production(api_key, latitude, longitude):
-    return _retrieve_data(api_key, latitude, longitude)
-
-
-def _retrieve_data(api_key, latitude, longitude, key_validator=None, api_key_reader=None):
-    data_retriever_controller = _create_controller(key_validator, api_key_reader)
-    api_response = data_retriever_controller.get(api_key, latitude, longitude)
-    return jsonify(api_response.__dict__)
-
-
-def _create_controller(key_validator, api_key_reader):
-    data_retriever = EngineFactory.create_data_retriever(api_key_reader)
-    return ApiFactory.create_data_retriever_controller(data_retriever, key_validator)
+    return app_core.retrieve_data_production(api_key, latitude, longitude)
 
 
 if __name__ == '__main__':
     if "disable_debug" in argv:
-        disable_debug = True
+        app_core.disable_debug = True
     app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ webapp/appcore.py
@@ -0,0 +1,52 @@
+from flask import jsonify
+from flask import abort
+from wa.factory.apifactory import ApiFactory
+from wa.factory.enginefactory import EngineFactory
+from wa.api.freeforall import FreeForAll
+from wa.extapi.citkeys import CitKeys
+
+
+class AppCore(object):
+    def __init__(self):
+        self.disable_debug = False
+
+    def retrieve_data_test(self, api_key, latitude_str, longitude_str):
+        if self._disable_debug():
+            abort(404)
+            return
+        result = ApiFactory.create_dummy_response(float(latitude_str), float(longitude_str))
+        return jsonify(result.__dict__)
+
+    def _disable_debug(self):
+        def _disable_uwsgi_debug():
+            try:
+                from uwsgi import opt
+                return "true" == opt["disable_debug"].decode()
+            except:
+                return False
+        return self.disable_debug or _disable_uwsgi_debug()
+
+    def retrieve_data_staging(self, api_key, latitude_str, longitude_str):
+        if self._disable_debug():
+            abort(404)
+            return
+        key_validator = FreeForAll()
+        api_key_reader = CitKeys()
+        return self._retrieve_data(
+            api_key, latitude_str, longitude_str, key_validator, api_key_reader
+        )
+
+    def retrieve_data_production(self, api_key, latitude_str, longitude_str):
+        return self._retrieve_data(api_key, latitude_str, longitude_str)
+
+    def _retrieve_data(
+        self, api_key, latitude_str, longitude_str, key_validator=None, api_key_reader=None
+    ):
+        data_retriever_controller = self._create_controller(key_validator, api_key_reader)
+        api_response = data_retriever_controller.get(api_key, latitude_str, longitude_str)
+        return jsonify(api_response.__dict__)
+
+
+    def _create_controller(self, key_validator, api_key_reader):
+        data_retriever = EngineFactory.create_data_retriever(api_key_reader)
+        return ApiFactory.create_data_retriever_controller(data_retriever, key_validator)
