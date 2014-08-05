00010ad234b839a94135a02946f44228f92b6059
Camilo Sanchez <camilin87@gmail.com>
2014-08-04 22:51:33 -0400

    add some logging at the api level

--- wa/extapi/forecastioretriever.py
+++ wa/extapi/forecastioretriever.py
@@ -3,6 +3,7 @@ from forecastio import load_forecast
 from wa.extapi.dataresponsebuilder import DataResponseBuilder
 from requests.exceptions import RequestException
 from wa.engine.transmissionerror import TransmissionError
+from logging import info
 
 
 class ForecastIoRetriever(DataRetriever):
@@ -12,11 +13,24 @@ class ForecastIoRetriever(DataRetriever):
 
     def retrieve(self, data_request):
         try:
+            self._log_data_request(data_request)
+
             datapoint = load_forecast(
                 self.api_key,
                 data_request.latitude,
                 data_request.longitude
             ).currently()
+
             return self.builder.build(datapoint)
         except RequestException as req_error:
             raise TransmissionError("TransmissionError: " + str(req_error)) from req_error
+
+    def _log_data_request(self, data_request):
+        info((
+            "ForecastIoRetriever.retrieve; " + 
+            "api_key='{0}', latitude={1:.2f}, longitude={2:.2f}"
+        ).format(
+            self.api_key,
+            data_request.latitude,
+            data_request.longitude
+        ))
