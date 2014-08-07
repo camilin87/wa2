0a374fc2fc98d0d500a2012b9883eca5288f7745
Camilo Sanchez <camilin87@gmail.com>
2014-08-06 21:41:41 -0400

    add some logging around forecastiohelper

--- test/forecastiohelper.py
+++ test/forecastiohelper.py
@@ -1,11 +1,22 @@
 from wa.extapi.citkeys import CitKeys
 from forecastio import load_forecast
+from logging import info
 
 
 class ForecastIoHelper(object):
     @staticmethod
     def call_api_directly(lat, lng):
         api_key = CitKeys().key()
+
+        info((
+            "ForecastIoHelper.call_api_directly; " +
+            "api_key='{0}', latitude={1:.2f}, longitude={2:.2f}"
+        ).format(
+            api_key,
+            lat,
+            lng
+        ))
+
         datapoint = load_forecast(api_key, lat, lng).currently()
         result = {
             "summary": datapoint.summary,
